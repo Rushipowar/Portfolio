8568a8f13dae33555c8e78d41e6aa41cd876cd16
Rushikesh Powar <114715746+Rushipowar@users.noreply.github.com>
2024-06-24 15:06:46 +0530

    Add files via upload

---
+++ src/components/DevelopmentBanner.jsx
@@ -0,0 +1,12 @@
+// DevelopmentBanner.js
+import React from "react";
+
+function DevelopmentBanner() {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 bg-red-600 text-white text-center py-6 font-bold text-lg">
+      This website is in the development phase.
+    </div>
+  );
+}
+
+export default DevelopmentBanner;
